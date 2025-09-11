b0939e873625c80cb4075dd42cbb5476437747df
Giddy-Up224 <128929355+Giddy-Up224@users.noreply.github.com>
2025-09-11 09:20:58 -0400

    Made config files dynamic

---
+++ copy_configs.py
@@ -0,0 +1,20 @@
+import shutil
+import os
+
+Import("env")
+
+custom_tft_setup = "lib/TFT_eSPI/User_Setup.h"
+custom_lv_conf = "lib/lvgl_conf/lv_conf.h"
+
+tft_dest = os.path.join(env['PROJECT_DIR'], ".pio", "libdeps", env['PIOENV'], "TFT_eSPI", "User_Setup.h")
+lvgl_dest = os.path.join(env['PROJECT_DIR'], ".pio", "libdeps", env['PIOENV'], "lvgl", "lv_conf.h")
+
+def before_build(source, target, env):
+    if os.path.exists(custom_tft_setup):
+        shutil.copyfile(custom_tft_setup, tft_dest)
+        print(f"Copied {custom_tft_setup} to {tft_dest}")
+    if os.path.exists(custom_lv_conf):
+        shutil.copyfile(custom_lv_conf, lvgl_dest)
+        print(f"Copied {custom_lv_conf} to {lvgl_dest}")
+
+env.AddPreAction("buildprog", before_build)
