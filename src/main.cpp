d5ed99b2294cc918e0f4b138b9ba52eef4a1b1cc
Giddy-Up224 <128929355+Giddy-Up224@users.noreply.github.com>
2025-09-22 06:52:34 -0400

    deactivated all lvgl

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <Arduino.h>
 #include <NimBLEDevice.h>
-#include <LVGL_CYD.h>
-#include <lvgl.h>
+//#include <LVGL_CYD.h>
+//#include <lvgl.h>
 
 // Project modules
 #include "config.h"
@@ -11,8 +11,8 @@
 //********************************************
 
 // Screen objects
-lv_obj_t* scr_main = nullptr;
-lv_obj_t* scr_scan = nullptr;
+//lv_obj_t* scr_main = nullptr;
+//lv_obj_t* scr_scan = nullptr;
 
 // BLE scanning
 NimBLEScan* pScan;
@@ -39,42 +39,42 @@ ScanCallbacks scanCallbacks;
 // Screens
 //********************************************
 // Creates a new obj to use as base screen
-lv_obj_t* new_screen(lv_obj_t* parent) {
-  lv_obj_t* obj = lv_obj_create(parent);
-  lv_obj_set_style_bg_opa(obj, LV_OPA_TRANSP, LV_PART_MAIN);
-  lv_obj_clear_flag(obj, LV_OBJ_FLAG_SCROLLABLE);
-  lv_obj_set_style_border_width(obj, 0, 0);
-  lv_obj_set_layout(obj, LV_LAYOUT_FLEX);
-  lv_obj_set_flex_flow(obj, LV_FLEX_FLOW_COLUMN);
-  lv_obj_set_flex_align(obj, LV_FLEX_ALIGN_CENTER, LV_FLEX_ALIGN_CENTER, LV_FLEX_ALIGN_CENTER);
-  lv_obj_set_style_pad_top(obj, 20, LV_PART_MAIN);
-  lv_obj_set_style_pad_row(obj, 10, LV_PART_MAIN);
-  return obj;
-}
-
-void add_discovered_device(lv_obj_t* btn_parent) {
-  lv_obj_t* device_btn = lv_btn_create(btn_parent);
-
-}
-
-void go_main() {
-  if (!scr_main) {
-    scr_main = new_screen(NULL);
-    lv_obj_set_size(scr_main, lv_disp_get_hor_res(NULL), lv_disp_get_ver_res(NULL));
-
-    lv_obj_t* scan_btn = lv_btn_create(scr_main);
-    lv_obj_align(scan_btn, LV_ALIGN_TOP_LEFT, -10, 10);
-    lv_obj_add_event_cb(scan_btn, [](lv_event_t* e) -> void {
-      scanForDevices();
-    }, LV_EVENT_CLICKED, NULL);
-
-    lv_obj_t* scan_btn_label = lv_label_create(scan_btn);
-    lv_label_set_text(scan_btn_label, "...");
-    lv_obj_align_to(scan_btn_label, scan_btn, LV_ALIGN_CENTER, 0, 0);
-  }
-
-  lv_screen_load(scr_main);
-}
+//lv_obj_t* new_screen(lv_obj_t* parent) {
+//  lv_obj_t* obj = lv_obj_create(parent);
+//  lv_obj_set_style_bg_opa(obj, LV_OPA_TRANSP, LV_PART_MAIN);
+//  lv_obj_clear_flag(obj, LV_OBJ_FLAG_SCROLLABLE);
+//  lv_obj_set_style_border_width(obj, 0, 0);
+//  lv_obj_set_layout(obj, LV_LAYOUT_FLEX);
+//  lv_obj_set_flex_flow(obj, LV_FLEX_FLOW_COLUMN);
+//  lv_obj_set_flex_align(obj, LV_FLEX_ALIGN_CENTER, LV_FLEX_ALIGN_CENTER, LV_FLEX_ALIGN_CENTER);
+//  lv_obj_set_style_pad_top(obj, 20, LV_PART_MAIN);
+//  lv_obj_set_style_pad_row(obj, 10, LV_PART_MAIN);
+//  return obj;
+//}
+//
+//void add_discovered_device(lv_obj_t* btn_parent) {
+//  lv_obj_t* device_btn = lv_btn_create(btn_parent);
+//
+//}
+//
+//void go_main() {
+//  if (!scr_main) {
+//    scr_main = new_screen(NULL);
+//    lv_obj_set_size(scr_main, lv_disp_get_hor_res(NULL), lv_disp_get_ver_res(NULL));
+//
+//    lv_obj_t* scan_btn = lv_btn_create(scr_main);
+//    lv_obj_align(scan_btn, LV_ALIGN_TOP_LEFT, -10, 10);
+//    lv_obj_add_event_cb(scan_btn, [](lv_event_t* e) -> void {
+//      scanForDevices();
+//    }, LV_EVENT_CLICKED, NULL);
+//
+//    lv_obj_t* scan_btn_label = lv_label_create(scan_btn);
+//    lv_label_set_text(scan_btn_label, "...");
+//    lv_obj_align_to(scan_btn_label, scan_btn, LV_ALIGN_CENTER, 0, 0);
+//  }
+//
+//  lv_screen_load(scr_main);
+//}
 
 
 //********************************************
@@ -84,7 +84,7 @@ void setup() {
   Serial.begin(115200);
 
   // Initialize LVGL and display
-  LVGL_CYD::begin(SCREEN_ORIENTATION);
+  //LVGL_CYD::begin(SCREEN_ORIENTATION);
   // Setup BLE scanning
   pScan = NimBLEDevice::getScan();
   pScan->setScanCallbacks(&scanCallbacks);
@@ -98,7 +98,7 @@ void setup() {
 //********************************************
 void loop() {
   // Handle LVGL tasks
-  lv_task_handler();
+  //lv_task_handler();
   pScan->start(BLE_SCAN_TIME, false, true);
   Serial.printf("count: %d", pScan->getResults().getCount());
   delay(2000);
