043aed09a7ef61621fa3e1bdb3724dab224a9181
Giddy-Up224 <128929355+Giddy-Up224@users.noreply.github.com>
2025-09-22 16:56:12 -0400

    This scan logic works

--- src/main.cpp
+++ src/main.cpp
@@ -1,105 +1,49 @@
 #include <Arduino.h>
 #include <NimBLEDevice.h>
-//#include <LVGL_CYD.h>
-//#include <lvgl.h>
 
-// Project modules
-#include "config.h"
+int scanTime = 5 * 1000; // In milliseconds, 0 = scan forever
+NimBLEScan *pBLEScan;
 
-//********************************************
-// Global Variables
-//********************************************
+bool active = true;
 
-// Screen objects
-//lv_obj_t* scr_main = nullptr;
-//lv_obj_t* scr_scan = nullptr;
+class ScanCallbacks : public NimBLEScanCallbacks
+{
+  void onDiscovered(const NimBLEAdvertisedDevice *advertisedDevice) override {
+    Serial.printf("Discovered Advertised Device: %s \n", advertisedDevice->toString().c_str());
+  }
 
-// BLE scanning
-NimBLEScan* pScan;
+  void onResult(const NimBLEAdvertisedDevice *advertisedDevice) override {
+    Serial.printf("Advertised Device Result: %s \n", advertisedDevice->toString().c_str());
+  }
 
-//********************************************
-// Scanning logic
-//********************************************
-void scanForDevices() {
-  
+  void onScanEnd(const NimBLEScanResults &results, int reason) override {
+    Serial.print("Scan Ended; reason = ");
+    Serial.println(reason);
+    // active = !active;
+    pBLEScan->setActiveScan(active);
+    Serial.printf("scan start, active = %u\n", active);
+    pBLEScan->start(scanTime);
+  }
+} scanCallbacks;
+
+void startScan(uint32_t durationMs) {
+  static ScanCallbacks scanCallbacks; // make sure it stays alive
+
+  NimBLEScan *pBLEScan = NimBLEDevice::getScan();
+  pBLEScan->setScanCallbacks(&scanCallbacks);
+  pBLEScan->setActiveScan(true); // or false
+  pBLEScan->setInterval(100);
+  pBLEScan->setWindow(100);
+  pBLEScan->start(durationMs);
 }
 
-class ScanCallbacks : public NimBLEScanCallbacks {
-  public:
-  void onResult(const NimBLEAdvertisedDevice* advertisedDevice);
-};
-
-void ScanCallbacks::onResult(const NimBLEAdvertisedDevice* advertisedDevice) {
-  Serial.printf("BLE Device found: %s\n", advertisedDevice->toString().c_str());
-}
-
-ScanCallbacks scanCallbacks;
-
-//********************************************
-// Screens
-//********************************************
-// Creates a new obj to use as base screen
-//lv_obj_t* new_screen(lv_obj_t* parent) {
-//  lv_obj_t* obj = lv_obj_create(parent);
-//  lv_obj_set_style_bg_opa(obj, LV_OPA_TRANSP, LV_PART_MAIN);
-//  lv_obj_clear_flag(obj, LV_OBJ_FLAG_SCROLLABLE);
-//  lv_obj_set_style_border_width(obj, 0, 0);
-//  lv_obj_set_layout(obj, LV_LAYOUT_FLEX);
-//  lv_obj_set_flex_flow(obj, LV_FLEX_FLOW_COLUMN);
-//  lv_obj_set_flex_align(obj, LV_FLEX_ALIGN_CENTER, LV_FLEX_ALIGN_CENTER, LV_FLEX_ALIGN_CENTER);
-//  lv_obj_set_style_pad_top(obj, 20, LV_PART_MAIN);
-//  lv_obj_set_style_pad_row(obj, 10, LV_PART_MAIN);
-//  return obj;
-//}
-//
-//void add_discovered_device(lv_obj_t* btn_parent) {
-//  lv_obj_t* device_btn = lv_btn_create(btn_parent);
-//
-//}
-//
-//void go_main() {
-//  if (!scr_main) {
-//    scr_main = new_screen(NULL);
-//    lv_obj_set_size(scr_main, lv_disp_get_hor_res(NULL), lv_disp_get_ver_res(NULL));
-//
-//    lv_obj_t* scan_btn = lv_btn_create(scr_main);
-//    lv_obj_align(scan_btn, LV_ALIGN_TOP_LEFT, -10, 10);
-//    lv_obj_add_event_cb(scan_btn, [](lv_event_t* e) -> void {
-//      scanForDevices();
-//    }, LV_EVENT_CLICKED, NULL);
-//
-//    lv_obj_t* scan_btn_label = lv_label_create(scan_btn);
-//    lv_label_set_text(scan_btn_label, "...");
-//    lv_obj_align_to(scan_btn_label, scan_btn, LV_ALIGN_CENTER, 0, 0);
-//  }
-//
-//  lv_screen_load(scr_main);
-//}
-
-
-//********************************************
-// Setup Function
-//********************************************
 void setup() {
   Serial.begin(115200);
-
-  // Initialize LVGL and display
-  //LVGL_CYD::begin(SCREEN_ORIENTATION);
-  // Setup BLE scanning
-  pScan = NimBLEDevice::getScan();
-  pScan->setScanCallbacks(&scanCallbacks);
-  pScan->setInterval(BLE_SCAN_INTERVAL);
-  pScan->setWindow(BLE_SCAN_WINDOW);
-  pScan->setActiveScan(true);
+  Serial.println("Scanning...");
+  NimBLEDevice::init("active-passive-scan");
+  startScan(scanTime);
 }
 
-//********************************************
-// Main Loop
-//********************************************
 void loop() {
-  // Handle LVGL tasks
-  //lv_task_handler();
-  pScan->start(BLE_SCAN_TIME, false, true);
-  Serial.printf("count: %d", pScan->getResults().getCount());
-  delay(2000);
+  // nothing to do here...
 }
