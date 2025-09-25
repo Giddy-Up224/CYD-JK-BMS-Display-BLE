bf725979256f1cfbf9036828acba3d03310d5a41
Andy Schrock <128929355+Giddy-Up224@users.noreply.github.com>
2025-09-24 20:28:02 -0400

    fixed lv_conf.h destination path and added error notifications

--- copy_configs.py
+++ copy_configs.py
@@ -13,14 +13,18 @@ custom_tft_setup = "lib/TFT_eSPI/User_Setup.h"
 custom_lv_conf = "lib/lvgl_conf/lv_conf.h"
 
 tft_dest = os.path.join(env['PROJECT_DIR'], ".pio", "libdeps", env['PIOENV'], "TFT_eSPI", "User_Setup.h")
-lvgl_dest = os.path.join(env['PROJECT_DIR'], ".pio", "libdeps", env['PIOENV'], "lvgl", "lv_conf.h")
+lvgl_dest = os.path.join(env['PROJECT_DIR'], ".pio", "libdeps", env['PIOENV'], "lv_conf.h")
 
 def before_build(source, target, env):
     if os.path.exists(custom_tft_setup):
         shutil.copyfile(custom_tft_setup, tft_dest)
         print(f"Copied {custom_tft_setup} to {tft_dest}")
+    else:
+        print(f"{custom_tft_setup} does not exist!")
     if os.path.exists(custom_lv_conf):
         shutil.copyfile(custom_lv_conf, lvgl_dest)
         print(f"Copied {custom_lv_conf} to {lvgl_dest}")
+    else:
+        print(f"{custom_lv_conf} does not exist!")
 
 env.AddPreAction("buildprog", before_build)
